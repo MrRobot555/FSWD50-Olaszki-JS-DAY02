f1489a216969bc14eeb405d1a14811c67258682e
nicoarl <nico.adelmann@hotmail.com>
2018-09-17 13:41:42 +0200

    date exercise

--- Intermediate_exercise2_adelmann/script.js
+++ Intermediate_exercise2_adelmann/script.js
@@ -9,7 +9,44 @@ function average_sum() {
 }
 
 function minutes_into_hours() {
-	var minutes = Number(document.getElementById("minutes").value);
-	var hours = Math.round(minutes/60) +" hours : "+ minutes%60 +" minutes";
-	document.getElementById("number_in_hours").innerHTML = hours;
+    var minutes = Number(document.getElementById("minutes").value);
+    var hours = Math.round(minutes / 60) + " hours : " + minutes % 60 + " minutes";
+    document.getElementById("number_in_hours").innerHTML = hours;
+}
+
+ var yesterdays_date = function() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+
+
+
+
+    var yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    var y_dd = yesterday.getDate();
+    var y_mm = yesterday.getMonth() +1 ;
+    var y_yyyy = yesterday.getFullYear();
+    if (y_dd < 10) {
+        y_dd = '0' + y_dd
+    }
+
+    if (y_mm < 10) {
+        y_mm = '0' + y_mm
+    }    
+
+
+    today = dd + '/' + mm + '/' + yyyy;
+    yesterday = y_dd + '/' + y_mm + '/' + y_yyyy;
+	document.getElementById("output_todaysdate").innerHTML = today;
+	document.getElementById("output_yesterdaysdate").innerHTML = yesterday;
+
 }

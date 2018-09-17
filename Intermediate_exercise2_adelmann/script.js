f68a51dafb2e5f76db42c4088dc9399d5cf53e2f
nicoarl <nico.adelmann@hotmail.com>
2018-09-17 11:53:40 +0200

    advanced 1

--- Intermediate_exercise2_adelmann/script.js
+++ Intermediate_exercise2_adelmann/script.js
@@ -1,9 +1,15 @@
-function average_sum(){
-var a = Number(document.getElementById("first_num").value);
-var b = Number(document.getElementById("second_num").value);
-var c = Number(document.getElementById("third_num").value);
-var average = (a+b+c)/3;
-var sum = a+b+c;
-document.getElementById("sum").innerHTML = sum;
-document.getElementById("average").innerHTML = average;
+function average_sum() {
+    var a = Number(document.getElementById("first_num").value);
+    var b = Number(document.getElementById("second_num").value);
+    var c = Number(document.getElementById("third_num").value);
+    var average = (a + b + c) / 3;
+    var sum = a + b + c;
+    document.getElementById("sum").innerHTML = sum;
+    document.getElementById("average").innerHTML = average;
+}
+
+function minutes_into_hours() {
+	var minutes = Number(document.getElementById("minutes").value);
+	var hours = Math.round(minutes/60) +" hours : "+ minutes%60 +" minutes";
+	document.getElementById("number_in_hours").innerHTML = hours;
 }

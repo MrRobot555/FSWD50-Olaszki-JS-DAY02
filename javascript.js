06329eb0f3c41d34b9905302e526a809282d7c77
Thomas Olaszki <olaszky@gmail.com>
2018-09-17 09:59:28 +0200

    first excercise

---
+++ javascript.js
@@ -0,0 +1,5 @@
+function crystalGazer(childrenNr, partnerName, location, jobTitle) {
+    document.write("You will be a "+jobTitle+" in "+location+" and married to "+partnerName+" with "+childrenNr+" children");
+}
+
+crystalGazer(4, "Josef", "Rome", "Web Developer");
